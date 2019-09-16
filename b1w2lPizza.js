f0c6ac95ea5fcca052e74124551c0b2be91de813
Marijn <marijndbesten@gmail.com>
2019-09-16 09:08:52 +0200

    Blegh

--- b1w2lPizza.js
+++ b1w2lPizza.js
@@ -3,7 +3,7 @@ var a = parseInt(prompt("Hoeveel small pizzas wil je? : €5,00"));
 var b = parseInt(prompt("Hoeveel medium pizzas wil je? : €8,00"));
 var c = parseInt(prompt("Hoeveel large pizzas wil je? : €12,00"));
 
-var d = parseInt(a * 5);
+var d = a * 5;
 var e = parseInt(b * 8);
 var f = parseInt(c * 12);
 
